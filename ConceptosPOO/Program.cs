f40a5e76e6cbaef2ab65b7a2bb041dcbd9011c4d
Fidel Antonio Gomez Mitsui <78324379+Zeykroz13@users.noreply.github.com>
2023-03-21 23:10:51 -0600

    Classes: Employee, SalaryEmployee & CommissionEmployee, implemented.

--- ConceptosPOO/Program.cs
+++ ConceptosPOO/Program.cs
@@ -11,3 +11,30 @@ catch (Exception error)
 	Console.WriteLine(error.Message);
 }
 
+
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Juan",
+    LastName = "Perez",
+    BirthDate = new Date(1, 1, 1980),
+    HiringDate = new Date(1, 1, 2010),
+    IsActive = true,
+    Salary = 1034345600.45M
+};
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Lola",
+    LastName = "Monita",
+    BirthDate = new Date(13, 11, 2001),
+    HiringDate = new Date(11, 12, 2020),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+
+Console.WriteLine(employee1);
+Console.WriteLine(employee2);
+
