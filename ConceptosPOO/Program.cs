1cf2d568a4ea2c995ccaa0ae6372ef849f9807ed
Fidel Antonio Gomez Mitsui <78324379+Zeykroz13@users.noreply.github.com>
2023-03-21 21:33:46 -0600

    Practica Conceptos POO

---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConceptosPOO;
+
+try
+{
+	Console.WriteLine(new Date(29, 2, 2024));
+	Console.WriteLine(new Date(02, 9, 2024));
+    Console.WriteLine(new Date(30, 11, 2024));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
+
