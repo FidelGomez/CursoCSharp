424b4d64b46e88d62af6f6dc89c1f6941bcfb017
Fidel Antonio Gomez Mitsui <78324379+Zeykroz13@users.noreply.github.com>
2023-03-24 18:50:10 -0600

    Logging logic implemented

--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -33,6 +33,8 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 builder.Services.AddTransient<SeedDB>();
 builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddScoped<ICombosHelper, CombosHelper>();
+builder.Services.AddScoped<IBlobHelper, BlobHelper>();
 
 var app = builder.Build();
 SeedData();
