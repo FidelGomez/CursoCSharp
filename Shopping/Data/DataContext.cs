0180407b16ebf4467926ad8023ca3ac224053746
Fidel Antonio Gomez Mitsui <78324379+Zeykroz13@users.noreply.github.com>
2023-03-25 01:08:59 -0600

    Add product

--- Shopping/Data/DataContext.cs
+++ Shopping/Data/DataContext.cs
@@ -15,6 +15,10 @@ namespace Shopping.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<State> States { get; set; }
         public DbSet<City> Cities { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -23,6 +27,9 @@ namespace Shopping.Data
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<State>().HasIndex("Name","CountryId").IsUnique();
             modelBuilder.Entity<City>().HasIndex("Name","StateId").IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<ProductCategory>().HasIndex("ProductId", "CategoryId").IsUnique();
+
         }
     }
 }
